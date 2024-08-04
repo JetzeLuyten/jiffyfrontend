6a086e8ead3280d6ac2ec11f2a217adeb6fd7f13
Jetze Luyten <r0785521@student.thomasmore.be>
2024-08-04 21:17:40 +0200

    Stylized the home-component

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -8,12 +8,13 @@ import { ServicesService } from '../../services/service.services';
 import { Router } from '@angular/router';
 import { LoginComponent } from "../../auth0/login/login.component";
 import { AuthService } from '@auth0/auth0-angular';
+import { ServiceShopComponent } from "../../service/service-shop/service-shop.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MenuComponent, ServicesComponent, LoginComponent],
+  imports: [CommonModule, MenuComponent, ServicesComponent, LoginComponent, ServiceShopComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -21,7 +22,7 @@ export class HomeComponent {
   services$: Observable<Service[]> = new Observable<Service[]>();
   isAuthenticated = signal(false);
 
-  constructor(private serviceService: ServicesService, private authService: AuthService) {}
+  constructor(private serviceService: ServicesService, private authService: AuthService, private router: Router) {}
   
   ngOnInit(): void {
     this.authService.isAuthenticated$.subscribe((auth) => {
@@ -29,4 +30,24 @@ export class HomeComponent {
     });
     this.services$ = this.serviceService.getServices();
   }
-}
+
+  navigateToShop() {
+    if (!this.isAuthenticated) {
+      this.router.navigate(['/shopguest']);
+    }
+    else{
+      this.router.navigate(["/shop"]);
+    }
+  }
+
+  navigateToLogin(): void {
+      this.authService.loginWithRedirect({
+        appState: {
+          target: '',
+        },
+        authorizationParams: {
+          prompt: 'login', //other possibilities: https://auth0.github.io/auth0-spa-js/interfaces/AuthorizationParams.html#prompt,
+        },
+      });
+    }
+  }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
     production: false, // this is true in the production environment
     AUTH0_DOMAIN: "dev-pk3maww8cgylreqx.us.auth0.com",
     AUTH0_CLIENT_ID: "bbPs7JATpF4qoDtwqoG346ISNDb6vDYZ",
-    redirectUri: "http://localhost:5878/callback",
-    api_url: "https://localhost:6587/api",
-    home_url: "http://localhost:5878",
-    AUTH0_AUDIENCE: "https://localhost:6587/"
+    redirectUri: "https://jiffyservice.netlify.app/callback",
+    api_url: "https://jiffybackend.azurewebsites.net/",
+    home_url: "https://jiffyservice.netlify.app/",
+    AUTH0_AUDIENCE: "https://jiffybackend.azurewebsites.net/"
   };
