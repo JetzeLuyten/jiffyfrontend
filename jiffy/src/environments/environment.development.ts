3d013701cec4427976c5802b3a52794ff1254f9a
Jetze Luyten <r0785521@student.thomasmore.be>
2024-08-05 17:59:21 +0200

    Replacing localhost with online equivelant

--- jiffy/src/environments/environment.development.ts
+++ jiffy/src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
     production: true, // this is true in the production environment
     AUTH0_DOMAIN: "dev-pk3maww8cgylreqx.us.auth0.com",
     AUTH0_CLIENT_ID: "bbPs7JATpF4qoDtwqoG346ISNDb6vDYZ",
-    redirectUri: "https://jiffyservice.netlify.app//callback",
+    redirectUri: "https://jiffyservice.netlify.app/callback",
     api_url: "https://jiffybackend.azurewebsites.net/",
     home_url: "https://jiffyservice.netlify.app/",
     AUTH0_AUDIENCE: "https://jiffybackend.azurewebsites.net/"
