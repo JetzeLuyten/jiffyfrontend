9e6fd041245fc9fb9cc19ad28d803500c3480c61
Jetze Luyten <r0785521@student.thomasmore.be>
2024-08-07 13:32:33 +0200

    Environment change

--- jiffy/src/environments/environment.ts
+++ jiffy/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     AUTH0_DOMAIN: "dev-pk3maww8cgylreqx.us.auth0.com",
     AUTH0_CLIENT_ID: "bbPs7JATpF4qoDtwqoG346ISNDb6vDYZ",
     redirectUri: "https://jiffyservices.netlify.app/callback",
-    api_url: "https://jiffybackend.azurewebsites.net/api",
+    api_url: "https://jiffybackend.azurewebsites.net",
     home_url: "https://jiffyservices.netlify.app",
     AUTH0_AUDIENCE: "https://jiffybackend.azurewebsites.net/"
   };
