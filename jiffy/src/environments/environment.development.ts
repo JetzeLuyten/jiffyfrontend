8687580d0b2b19ca7247efefe1b919eee45846ff
Jetze Luyten <r0785521@student.thomasmore.be>
2024-08-09 13:51:33 +0200

    Environment change

--- jiffy/src/environments/environment.development.ts
+++ jiffy/src/environments/environment.development.ts
@@ -3,8 +3,8 @@ export const environment = {
     production: true, // this is true in the production environment
     AUTH0_DOMAIN: "dev-pk3maww8cgylreqx.us.auth0.com",
     AUTH0_CLIENT_ID: "bbPs7JATpF4qoDtwqoG346ISNDb6vDYZ",
-    redirectUri: "https://jiffyservices.netlify.app",
+    redirectUri: "https://jiffyservices.netlify.app/",
     api_url: "https://jiffybackend.azurewebsites.net",
-    home_url: "https://jiffyservices.netlify.app",
+    home_url: "https://jiffyservices.netlify.app/",
     AUTH0_AUDIENCE: "https://jiffybackend.azurewebsites.net/"
   };
